a1c71f7b764fd03503241e9cce3b7796a36c6cb6
Nilesh Raj <47133959+nilesh7808@users.noreply.github.com>
2026-02-13 19:27:39 +0530

    Create IsAnagramOrNot.java
    Program to check the given strings are Anagram or not

---
+++ IsAnagramOrNot.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Anagram {
+    public static boolean isAnagram(String s1, String s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        // sorting string to lowercase, converting and storing them to array
+        char[] newS1 = s1.toLowerCase().toCharArray();
+        char[] newS2 = s2.toLowerCase().toCharArray();
+        Arrays.sort(newS1);
+        Arrays.sort(newS2);
+
+       // string comparison
+       return Arrays.equals(newS1, newS2);
+    }
+    
+    public static void main(String[] args) {
+        Anagram anagram = new Anagram();
+        System.out.println(anagram.isAnagram("cat","act"));
+    }
+}
