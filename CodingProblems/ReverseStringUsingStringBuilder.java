8f61d0a1e8f0eb5cfd3fac41c200bafa293cbf6d
Nilesh Raj <rajnilesh088@gmail.com>
2026-02-18 08:19:39 +0530

    New Code Added 18/02/2026

---
+++ CodingProblems/ReverseStringUsingStringBuilder.java
@@ -0,0 +1,15 @@
+package CodingProblems;
+import java.util.Scanner;
+
+public class ReverseStringUsingStringBuilder {
+    public static void main(String[] args) {
+        System.out.print("Enter a name that you want to reverse : ");
+        Scanner sc = new Scanner(System.in);
+        String originalString = sc.nextLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(originalString);
+        stringBuilder.reverse();
+        System.out.println("The final reversed String will be: " + stringBuilder);
+        System.out.println("Original string is : " + originalString);
+    }
+}
